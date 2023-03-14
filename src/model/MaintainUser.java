369f908c308f22612eb61f60d5e94788bffd1535
NatalieRDean <natalierdean1@gmail.com>
2023-03-14 12:36:30 -0400

    Added CSV example to project to be modified.

---
+++ src/model/MaintainUser.java
@@ -0,0 +1,68 @@
+package model;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import com.csvreader.CsvReader;
+import com.csvreader.CsvWriter;
+import objects.User;
+
+public class MaintainUser {
+	public ArrayList<User> users = new ArrayList<User>();
+	public String path;
+	
+	public void load(String path) throws Exception{
+		CsvReader reader = new CsvReader(path); 
+		reader.readHeaders();
+		
+		while(reader.readRecord()){ 
+			User user = new User();
+			//name,id,email,password
+			user.setName(reader.get("name"));
+			user.setId(Integer.valueOf(reader.get("id")));
+			user.setEmail(reader.get("email"));
+			user.setPassword(reader.get("password"));
+			users.add(user);
+		}
+	}
+	
+	public void update(String path) throws Exception{
+		try {		
+				CsvWriter csvOutput = new CsvWriter(new FileWriter(path, false), ',');
+				//name,id,email,password
+				csvOutput.write("name");
+				csvOutput.write("id");
+		    	csvOutput.write("email");
+				csvOutput.write("password");
+				csvOutput.endRecord();
+
+				// else assume that the file already has the correct header line
+				// write out a few records
+				for(User u: users){
+					csvOutput.write(u.getName());
+					csvOutput.write(String.valueOf(u.getId()));
+					csvOutput.write(u.getEmail());
+					csvOutput.write(u.getPassword());
+					csvOutput.endRecord();
+				}
+				csvOutput.close();
+			
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+	}
+	public static void main(String [] args) throws Exception{
+		String path = "C:\\Users\\wangs\\Desktop\\EECS3311\\project\\user.csv";
+		MaintainUser maintain = new MaintainUser();
+	
+		maintain.load(path);
+		for(User u: maintain.users){
+			System.out.println(u.toString());
+		}
+		
+		User newUser = new User("t4", 4, "t4@yorku.ca", "t4t4");
+		maintain.users.add(newUser);
+		
+		maintain.update(path);
+	}
+}
---
+++ src/objects/User.java
@@ -0,0 +1,59 @@
+package objects;
+
+public class User {
+	public String name;
+	public int id;
+	public String email;
+	public String password;
+	
+	public User(String name, int id, String email, String password) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.email = email;
+		this.password = password;
+	}
+	
+	public User(){
+		super();
+	}
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", id=" + id + ", email=" + email + ", password=" + password + "]";
+	}
+	
+}
