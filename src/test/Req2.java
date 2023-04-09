7587143a049a9b7ed4dff2355e4d543c5c8232ea
suhasid098 <suhasid098@gmail.com>
2023-04-09 00:57:04 -0400

    Merge branch 'main' of https://github.com/NatalieRDean/eecs3311project.git into main

--- src/test/Req2.java
+++ src/test/Req2.java
@@ -14,10 +14,11 @@ import controllers.UserController;
 import objects.Manager;
 import views.*;
 import views.RegisterView;
+
 // might have to add clearManager for the ArrayList in ManagerController
 // and clear() for the csv
 public class Req2 {
-	
+
 	@Test // Super Manager logs in and generates management account
 	public void test1() {
 		ManagerController.resetManager();
@@ -26,7 +27,7 @@ public class Req2 {
 		ManagerController.logInManager("SuperManager", "Aa!1");
 		ManagerActionsView mv1 = new ManagerActionsView(null);
 		ManageAccountsView mv2 = new ManageAccountsView(null);
-		
+
 		Manager newManager = ManagerController.generateManager();
 		assertNotNull(newManager); // means manager has been generated
 		ManagerController.logOutManager();
@@ -43,49 +44,49 @@ public class Req2 {
 		Manager newManager2 = ManagerController.generateManager();
 		Manager newManager3 = ManagerController.generateManager();
 
-
 		ArrayList<Manager> m = ManagerController.getManagers();
-	
-		for (int i = 0; i < m.size()-1; i++) { // dont include last manager in the list for comparison
-			//Manager3 does not share the same password or name as the previous passwords
+
+		for (int i = 0; i < m.size() - 1; i++) { // dont include last manager in the list for comparison
+			// Manager3 does not share the same password or name as the previous passwords
 			assertFalse(m.get(i).getName() == newManager3.getName());
 			assertFalse(m.get(i).getPassword() == newManager3.getPassword());
 		}
-		
+
 		ManagerController.resetManager();
 		ManagerController.clear();
 	}
-	@Test // Manager can have either a strong generated password or a strong pin (Builder pattern)
+
+	@Test // Manager can have either a strong generated password or a strong pin (Builder
+			// pattern)
 	public void test3() {
 		ManagerController.logInManager("SuperManager", "Aa!1");
 		Manager newManager = ManagerController.generateManager();
 		ManagerController.logOutManager();
 
-		assertEquals(8,newManager.getPassword().length());
-		
+		assertEquals(8, newManager.getPassword().length());
+
 		ManagerController.logInManager("SuperManager", "Aa!1");
 		Manager newManager2 = ManagerController.generateManagerWithPinPassword();
 		ManagerController.logOutManager();
-		
-		assertEquals(4,newManager2.getPassword().length());		
-		
+
+		assertEquals(4, newManager2.getPassword().length());
+
 		ManagerController.resetManager();
 		ManagerController.clear();
 	}
-
 
 	@Test // Account generation is NOT enabled for non-Super Managers
 	public void test4() {
 		ManagerController.logInManager("SuperManager", "Aa!1");
 		Manager manager = ManagerController.generateManagerWithPinPassword();
-		
+
 		// login with newly generated manager
 		ManagerController.logInManager(manager.getName(), manager.getPassword());
-		
+
 		Manager generatedManager = ManagerController.generateManager();
 		assertNull(generatedManager); // null beacuse only superManager can generate a manager
 		ManagerController.logOutManager();
-		
+
 		ManagerController.resetManager();
 		ManagerController.clear();
 	}
@@ -118,11 +119,7 @@ public class Req2 {
 		ManagerController.clear();
 		LotController.resetLots();
 		LotController.clear();
-		
 
 	}
-	
-	
 
 }
-
